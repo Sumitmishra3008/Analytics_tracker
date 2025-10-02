6e9a4816f15c463c90ab4a08da4defca4adfc3c5
Sumit Mishra <97983634+Sumitmishra3008@users.noreply.github.com>
2025-10-03 01:43:13 +0530

    event model modified to auto fill timestamp at dataentry point

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 
-const eventSchema = new mongoose.Schema(
-  {
-    event_id: { type: String, default: uuidv4, unique: true },
-    user_id: { type: String, required: true },
-    event_name: { type: String, required: true },
-    event_data: { type: mongoose.Schema.Types.Mixed, default: {} },
-    timestamp: { type: Date },
-  },
-  { timestamps: true }
-);
+const eventSchema = new mongoose.Schema({
+  event_id: { type: String, default: uuidv4, unique: true },
+  user_id: { type: String, required: true },
+  event_name: { type: String, required: true },
+  event_data: { type: mongoose.Schema.Types.Mixed, default: {} },
+  timestamp: { type: Date, default: Date.now },
+});
 
 const Event = mongoose.model("Event", eventSchema);
 
